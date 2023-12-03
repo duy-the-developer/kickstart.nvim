4e61fa8897cd18982332febc55e16fa32783e4e2
Ngoc Duy Anh Nguyen <duythedeveloper@gmail.com>
2023-12-03 01:39:50 -0500

    coding + lsp + treesitter

--- lua/custom/plugins/coding.lua
+++ lua/custom/plugins/coding.lua
@@ -1,5 +1,5 @@
 return {
-	-- 'hrsh7th/nvim-cmp',
+  -- 'hrsh7th/nvim-cmp',
   {
     -- Autocompletion
     "hrsh7th/nvim-cmp",
@@ -14,9 +14,57 @@ return {
       -- Adds a number of user-friendly snippets
       'rafamadriz/friendly-snippets',
     },
+    config = function()
+      local cmp = require 'cmp'
+      require('luasnip').setup({
+      })
+      require('luasnip.loaders.from_vscode').lazy_load()
+      cmp.setup({
+      snippet = {
+	expand = function(args)
+	  luasnip.lsp_expand(args.body)
+	end,
+      },
+      completion = {
+	completeopt = 'menu,menuone,noinsert'
+      },
+      mapping = cmp.mapping.preset.insert {
+	['<C-n>'] = cmp.mapping.select_next_item(),
+	['<C-p>'] = cmp.mapping.select_prev_item(),
+	['<C-d>'] = cmp.mapping.scroll_docs(-4),
+	['<C-f>'] = cmp.mapping.scroll_docs(4),
+	['<C-Space>'] = cmp.mapping.complete {},
+	['<CR>'] = cmp.mapping.confirm {
+	  behavior = cmp.ConfirmBehavior.Replace,
+	  select = true,
+	},
+	['<Tab>'] = cmp.mapping(function(fallback)
+	  if cmp.visible() then
+	    cmp.select_next_item()
+	  elseif luasnip.expand_or_locally_jumpable() then
+	    luasnip.expand_or_jump()
+	  else
+	    fallback()
+	  end
+	end, { 'i', 's' }),
+	['<S-Tab>'] = cmp.mapping(function(fallback)
+	  if cmp.visible() then
+	    cmp.select_prev_item()
+	  elseif luasnip.locally_jumpable(-1) then
+	    luasnip.jump(-1)
+	  else
+	    fallback()
+	  end
+	end, { 'i', 's' }),
+      },
+      sources = {
+	{ name = 'nvim_lsp' },
+	{ name = 'luasnip' },
+      },
+    })
+    end
   },
 
-	-- 'numToStr/Comment.nvim'
-	{ "numToStr/Comment.nvim", opts = {} },
-
+  -- 'numToStr/Comment.nvim'
+  { "numToStr/Comment.nvim", opts = {} },
 }
