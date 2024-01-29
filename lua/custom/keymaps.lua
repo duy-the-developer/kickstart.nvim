1cc4e2e4647b675981e61ed7bd9cd1d5f68f3f4d
Ngoc Duy Anh Nguyen <duythedeveloper@gmail.com>
2024-01-28 22:08:53 -0500

    more QOL keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -17,11 +17,11 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = '*',
 })
 
 -- [[ Telescope specific keymaps ]]
@@ -29,35 +29,35 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- Telescope live_grep in git root
 -- Function to find the git root directory based on the current buffer's path
 local function find_git_root()
-  -- Use the current buffer's path as the starting point for the git search
-  local current_file = vim.api.nvim_buf_get_name(0)
-  local current_dir
-  local cwd = vim.fn.getcwd()
-  -- If the buffer is not associated with a file, return nil
-  if current_file == '' then
-    current_dir = cwd
-  else
-    -- Extract the directory from the current file's path
-    current_dir = vim.fn.fnamemodify(current_file, ':h')
-  end
+	-- Use the current buffer's path as the starting point for the git search
+	local current_file = vim.api.nvim_buf_get_name(0)
+	local current_dir
+	local cwd = vim.fn.getcwd()
+	-- If the buffer is not associated with a file, return nil
+	if current_file == '' then
+		current_dir = cwd
+	else
+		-- Extract the directory from the current file's path
+		current_dir = vim.fn.fnamemodify(current_file, ':h')
+	end
 
-  -- Find the Git root directory from the current file's path
-  local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')[1]
-  if vim.v.shell_error ~= 0 then
-    print 'Not a git repository. Searching on current working directory'
-    return cwd
-  end
-  return git_root
+	-- Find the Git root directory from the current file's path
+	local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')[1]
+	if vim.v.shell_error ~= 0 then
+		print 'Not a git repository. Searching on current working directory'
+		return cwd
+	end
+	return git_root
 end
 
 -- Custom live_grep function to search in git root
 local function live_grep_git_root()
-  local git_root = find_git_root()
-  if git_root then
-    require('telescope.builtin').live_grep {
-      search_dirs = { git_root },
-    }
-  end
+	local git_root = find_git_root()
+	if git_root then
+		require('telescope.builtin').live_grep {
+			search_dirs = { git_root },
+		}
+	end
 end
 
 vim.api.nvim_create_user_command('LiveGrepGitRoot', live_grep_git_root, {})
@@ -65,11 +65,11 @@ vim.api.nvim_create_user_command('LiveGrepGitRoot', live_grep_git_root, {})
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
+	-- You can pass additional configuration to telescope to change theme, layout, etc.
+	require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+		winblend = 10,
+		previewer = false,
+	})
 end, { desc = '[/] Fuzzily search in current buffer' })
 
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
@@ -130,6 +130,14 @@ keymap.set(n, '<C-down>', '<C-w>-')
 -- Buffer navigation
 keymap.set(n, '<S-h>', ':bprev<Return>')
 keymap.set(n, '<S-l>', ':bnext<Return>')
+
+-- VS Code like line movement
+keymap.set('v', 'J', ':m \'>+1<CR>gv=gv')
+keymap.set('v', 'K', ':m \'<-2<CR>gv=gv')
+
+-- Maintain visual selection on indent
+keymap.set('v', '<', '<gv')
+keymap.set('v', '>', '>gv')
 
 -- Override diagnostic keymaps
 keymap.set(n, ';d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,12 +1,13 @@
 return {
-  { 'rktjmp/lush.nvim' },
-  { 'metalelf0/jellybeans-nvim' },
-  { 'navarasu/onedark.nvim' },
-  {
-    'folke/tokyonight.nvim',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
-  },
+	{ 'rktjmp/lush.nvim' },
+	{ 'metalelf0/jellybeans-nvim' },
+	{ 'navarasu/onedark.nvim' },
+	{ 'folke/tokyonight.nvim' },
+	{
+		'Shatur/neovim-ayu',
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme 'tokyonight'
+		end,
+	}
 }
